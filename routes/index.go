bdeca0d5539c7fa3dcbf6ea2fd00b96e32971512
Bartek Paczesny <bartek@paczesny.pl>
2024-11-29 17:36:59 +0100

    add auth. patrick is gay

--- routes/index.go
+++ routes/index.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/not-a-phase-mom/go-fullstack-yourself/templates/pages"
 )
 
 func (router *Router) RegisterIndexRoutes(e *gin.Engine) {
@@ -10,5 +11,27 @@ func (router *Router) RegisterIndexRoutes(e *gin.Engine) {
 
 func (router *Router) handleIndex(c *gin.Context) {
 	token, err := c.Cookie("token")
-	//...
+	emptyUserIndex := pages.IndexPage(nil)
+
+	c.Writer.Header().Set("Content-Type", "text/html")
+
+	if err != nil {
+		emptyUserIndex.Render(c.Request.Context(), c.Writer)
+		return
+	}
+
+	id, err := router.R.GetValue(token)
+	if err != nil {
+		pages.ErrorPage(err.Error()).Render(c.Request.Context(), c.Writer)
+		return
+	}
+
+	user, err := router.Db.GetUserById(id)
+	if err != nil {
+		pages.ErrorPage(err.Error()).Render(c.Request.Context(), c.Writer)
+		return
+	}
+
+	component := pages.IndexPage(&user)
+	component.Render(c.Request.Context(), c.Writer)
 }
